1a02c7c8dfd532509d6ea24b0446e12d8be55300
fmg1395 <franes9514@gmail.com>
2020-06-24 00:29:25 -0600

    meh

--- ExamenDos/src/java/modelo/dao/GestorCorreo.java
+++ ExamenDos/src/java/modelo/dao/GestorCorreo.java
@@ -15,7 +15,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import modelo.Correo;
-import modelo.Usuario;
 
 
 public class GestorCorreo implements Serializable {
